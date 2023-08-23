1690bfd1fbca0731d57e5d1fc8d51f319e31842b
Vessel-Legends <79474598+Vessel-Legends@users.noreply.github.com>
2023-08-23 18:32:36 -0300

    fixed tooltip bug

--- ScrambledLibrary/doubleyolk.py
+++ ScrambledLibrary/doubleyolk.py
@@ -2,6 +2,14 @@ from googletrans import Translator
 import time, threading, threading, sys, os, itertools
 translator = Translator()
 def scramble(word: str, output_to_file: bool):
+    '''
+    ## Scramble a word using the Deviled scramble method.
+    ---
+    word: The word you want to scramble.
+    output_to_file: Weither to output the result to a file, or just print it to the console.
+    ---
+    Returns: A string with the output (if output_to_file is False), or a file with the original sentence and the scrambled one (if output_to_file is True).
+    '''
     done = False
     def animate():
         for c in itertools.cycle(['|', '/', '-', '\\']):
@@ -11,14 +19,6 @@ def scramble(word: str, output_to_file: bool):
             sys.stdout.flush()
             time.sleep(0.1)
         os.system("cls")
-    '''
-    ## Scramble a word using the DoubleYolk scramble method.
-    ---
-    word: The word you want to scramble.
-    output_to_file: Weither to output the result to a file, or just print it to the console.
-    ---
-    Returns: A string with the output (if output_to_file is False), or a file with the original sentence and the scrambled one (if output_to_file is True).
-    '''
     t = threading.Thread(target=animate)
     t.start()
     masstrans = translator.translate(translator.translate(translator.translate(translator.translate(translator.translate(translator.translate(translator.translate(translator.translate(translator.translate(translator.translate(translator.translate(translator.translate(translator.translate(translator.translate(translator.translate(translator.translate(word, dest="af").text, dest="ht").text, dest="ha").text, dest="haw").text, dest="iw").text, dest="hi").text, dest="hmn").text, dest="hu").text, dest="is").text, dest="ig").text, dest="id").text, dest="ga").text, dest="it").text, dest="ja").text, dest="jw").text, dest="en").text
