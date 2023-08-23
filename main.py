c52c1a577c746ff2a2edae19ee42cfac010bab62
Vessel-Legends <79474598+Vessel-Legends@users.noreply.github.com>
2023-08-23 17:23:04 -0300

    main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from ScrambledLibrary import deviled, sunnysideup, doubleyolk #you can do multiple methods or just 1
+
+text = input("some text goes here")
+
+#1st param is the text, 2nd param is weither to output it to a file or just the console.
+
+#you can use just one method here or use multiple.
+deviled.scramble(text, True)
